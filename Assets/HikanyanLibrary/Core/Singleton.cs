074c695b71d2a2af22cb69b4d7764cdda3a5b155
Hikanyan <hikanyan0131@gmail.com>
2026-05-11 01:24:35 +0900

    Add HikanyanLibrary Core

---
+++ Assets/HikanyanLibrary/Core/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HikanyanLibrary.Core
+{
+    public abstract class Singleton<T> : IDisposable where T : class
+    {
+        private static T _instance;
+        private static readonly object _lockObject = new object();
+        private static bool _isDisposed = false;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
+
+                lock (_lockObject)
+                {
+                    if (_instance != null || _isDisposed) return _instance;
+                    _instance = Activator.CreateInstance(typeof(T), true) as T;
+                        
+                    if (_instance is Singleton<T> singletonInstance)
+                    {
+                        singletonInstance.OnInstanceCreated();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void OnInstanceCreated() { }
+
+        public static void ResetInstance()
+        {
+            lock (_lockObject)
+            {
+                if (_instance is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+                _instance = null;
+                _isDisposed = false;
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            lock (_lockObject)
+            {
+                _isDisposed = true;
+                _instance = null;
+            }
+        }
+    }
+}
---
+++ Assets/HikanyanLibrary/Core/SingletonMonoBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace HikanyanLibrary.Core
+{
+    [DisallowMultipleComponent]
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    {
+        private static T _instance;
+        private static bool _isInitialized = false;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
+
+                _instance = FindFirstObjectByType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+                }
+
+                if (!_isInitialized)
+                {
+                    _isInitialized = true;
+                    _instance.OnInstanceCreated();
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void OnInstanceCreated() { }
+
+        /// <summary>
+        /// DontDestroyOnLoadを適用するかどうかを決定します
+        /// デフォルトはtrue。継承先でオーバーライドして制御してください
+        /// </summary>
+        protected virtual bool IsPersistent => true;
+
+        protected virtual void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this as T;
+
+            if (!_isInitialized)
+            {
+                _isInitialized = true;
+                OnInstanceCreated();
+            }
+
+            if (IsPersistent)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+                _isInitialized = false;
+            }
+        }
+
+        public static void ResetInstance()
+        {
+            if (_instance != null)
+            {
+                Destroy(_instance.gameObject);
+            }
+            _instance = null;
+            _isInitialized = false;
+        }
+    }
+}
